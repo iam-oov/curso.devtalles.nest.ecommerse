8aed5fd1b7ff3195bcb7c4b652b286b65e13d642
valdo <osvaldo@lvvdigital.com>
2024-06-26 14:55:07 -0600

    feat: add jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,30 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { User } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: { expiresIn: '1h' },
+        };
+      },
+    }),
+  ],
   exports: [TypeOrmModule],
 })
 export class AuthModule {}
---
+++ src/auth/interfaces/jwt-payload.interface.ts
@@ -0,0 +1,5 @@
+export interface JwtPayload {
+  email: string;
+
+  // [PEND]: agregar todo lo que queramos guardar
+}
