0e193cc136af132483f6020db1980ef83ba09c52
valdo <osvaldo@lvvdigital.com>
2024-07-01 13:41:57 -0600

    feat: add WS

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { FilesModule } from './files/files.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
+import { MessagesWsModule } from './messages-ws/messages-ws.module';
 
 @Module({
   imports: [
@@ -32,6 +33,7 @@ import { AuthModule } from './auth/auth.module';
     SeedModule,
     FilesModule,
     AuthModule,
+    MessagesWsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/messages-ws/messages-ws.gateway.ts
@@ -0,0 +1,22 @@
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+import { MessagesWsService } from './messages-ws.service';
+
+@WebSocketGateway({ cors: true })
+export class MessagesWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  constructor(private readonly messagesWsService: MessagesWsService) {}
+
+  async handleConnection(client: Socket) {
+    console.log('Cliente conectado:', client.id);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log('Cliente desconectado', client.id);
+  }
+}
